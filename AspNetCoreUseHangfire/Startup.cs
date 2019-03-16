ee840b5e212c50eabcddce249990b98be5dab619
毕殿卿 <bidianq@jinher.com>
2019-03-16 12:11:02 +0800

    添加项目文件。

---
+++ AspNetCoreUseHangfire/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Hangfire;
+using Hangfire.MySql.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreUseHangfire
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddHangfire(options =>
+            {
+                options.UseStorage(new MySqlStorage(Configuration.GetConnectionString("MySqlConnectionString")));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+
+            app.UseMvc();
+        }
+    }
+}
